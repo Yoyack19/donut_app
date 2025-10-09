eec2499a6ef0a8415fbf8f716aed49a522f6f05d
Yoyack19 <jacobcruz824@gmail.com>
2025-10-09 16:40:37 -0600

    AddBar added

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        //icono izquierda
+        leading: Icon(
+        Icons.menu,
+        color: Colors.grey[800]
+        ),
+        //icono derech
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(
+              Icons.person
+            ),
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          //Texto principal
+
+          //Pestañas
+
+          //contenido de pestañas
+
+          //Carrito
+
+        ],
+      ),
+    );
+  }
+}
